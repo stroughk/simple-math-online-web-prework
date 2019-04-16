9125c2d06d6b5c035cfc27ebea4f121bfe79801a
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-16 23:56:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,18 @@ puts  5+4
 end
 
 def subtraction(10, 5)
+  puts 10-5
 end
 
-def division(num1, num2)
+def division(10, 2)
+  puts 10/2
 end
 
-def multiplication(num1, num2)
+def multiplication(3, 2)
+  puts 3*2
 end
 
-def modulo(num1, num2)
+def modulo(10, 3)
 end
 
 def square_root(num)
