15bfe9de4ee200eaa25661f5ce14f9f172d5d6ed
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-16 23:58:52 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
-def addition(num1, num2)
+def addition(5, 4)
+puts  5+4
 end
 
-def subtraction(num1, num2)
+def subtraction(10, 5)
+  puts 10-5
 end
 
-def division(num1, num2)
+def division(10, 2)
+  puts 10/2
 end
 
-def multiplication(num1, num2)
+def multiplication(3, 2)
+  puts 3*2
 end
 
-def modulo(num1, num2)
+def modulo(10, 3)
+puts 10%3
 end
 
-def square_root(num)
+def square_root(81)
+  puts Math.sqrt(81)
 end
 
-def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+def order_of_operation(1, 2, 3, 4)
+  puts 4 + ((2 * 3 ) / 1 )
 end
