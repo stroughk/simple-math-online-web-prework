a1a4804bf74f2e2cf8b980c92e0d290277e0034c
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-16 23:58:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,11 +15,13 @@ def multiplication(3, 2)
 end
 
 def modulo(10, 3)
+puts 10%3
 end
 
-def square_root(num)
+def square_root(81)
+  puts Math.sqrt(81)
 end
 
-def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+def order_of_operation(1, 2, 3, 4)
+  puts 4 + ((2 * 3 ) / 1 )
 end
